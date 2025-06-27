16ea404f06eb983cd74efbfa072d87f1fbb3c5cb
Bryan Lee <bryanlee56098@gmail.com>
2025-06-27 04:32:57 -0700

    Resolve infinite blocking

--- src/app.py
+++ src/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify, request
 import os
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
 
 app = Flask(__name__);
 
 API_TOKEN = os.environ.get("API_TOKEN");
 
+MQTT_BROKER = "broker.hivemq.com";
+MQTT_PORT = 1883;
+MQTT_TOPIC = "8cb124f8c277c16ec0b2ee00569fd151a08e342b/esp32/relay";
+
 @app.route("/")
 def home():
-    return "API is LIVE!"
+    return "API is LIVE!";
 
 @app.route("/toggle", methods=["POST"])
 def toggle():
@@ -16,10 +20,12 @@ def toggle():
     if token != API_TOKEN:
         return jsonify({"error": "unauthorized"}), 403;
 
-    publish.single(
-        topic="8cb124f8c277c16ec0b2ee00569fd151a08e342b/esp32/relay",
-        payload="toggle",
-        hostname="broker.hivemq.com",
-        port=1883
-    );
-    return jsonify({"status": "ok"});
+    try:
+        client = mqtt.Client();
+        client.connect(MQTT_BROKER, MQTT_PORT, 60);
+        client.publish(MQTT_TOPIC, payload="toggle");
+        client.disconnect();
+
+        return jsonify({"status": "ok"}), 200;
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500;
